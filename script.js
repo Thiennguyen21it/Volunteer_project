7110adfbf171c7d21cd748b396de70e7cff27181
Na Cao <caomyna03@gmail.com>
2023-11-07 17:03:34 +0700

    update blog

---
+++ script.js
@@ -0,0 +1,83 @@
+var translations = {
+    en: {
+       home: "Home",
+       activities: "Activities",
+       achievement: "Achievement",
+       contributors: "Contributors",
+       QA: "QA & Contact",
+       project_title: "DEVELOPING NEW PA-CANG VILLAGE, CA DY COMMUNE, NAM GIANG DISTRICT",
+       project_content:"Charity project for the sustainable development of Co tu minority people in Pà-Ong Village aims to provide clean water, healthcare, education, and livelihood opportunities to improve the quality of life for the community. The project is focused on creating a sustainable future and making a difference in the lives of those who are less fortunate. The Co tu minority people in Pà-Ong Village face numerous challenges such as lack of access to clean water, inadequate healthcare facilities, poor education, and limited access to other basic amenities. The Charity project aims to alleviate these challenges and improve the quality of life for these people.",
+       sponser_button: "Be a contributor or sponsor",
+       program_title:"Teaching English for kids in the village",
+       program_content:"his is a program under our charitable project that aims to provide English language education to children in rural areas. The program is designed to equip children with the necessary skills to communicate effectively in English. Our team of experienced teachers will teach the basics of English language to the children.",
+       achivement_slogan:"Whatever it is that you care about, there will be a charity working on it. Charities help in lots of different ways",
+       achievement_sub:"The practice of charity means the voluntary giving of help to those in need, as a humanitairan act. There are a number of philosophies.",
+       achivement_title:"Fundraising For Children And Refugees From Donbass",
+       achivement_button:"View more",
+       blog_title:"Our Blog",
+       postBox_title:"Bringing a warm Tet to the highland people of Nam Giang",
+       postDecription: "These are communes bordering Se Kong province, Laos, in which Cho Chun and La Eê are the two most remote communes of Nam Giang district with more than 90% of the population being ethnic minorities, many of whom are ethnic minorities. The area has no mobile phone signal, the road to the commune center and villages is very difficult, and in the rainy season it is impossible to travel by motorbike.",
+       introBlog:"January 14-15, 2022, with the help of Nam Giang District Police, Provincial Police Youth Union coordinated with the People's Voice of Ho Chi Minh City, the Charity Association Little Sister Bo and Friends organized the Border Spring Program in 03 border communes of Cho Chun, La Eê and Dak Pring, Nam Giang district, Quang Nam province awarded 500 gifts worth more than 400 million VND for highland people."        ,
+       introBlog1: "These are communes bordering Se Kong province, Laos, including Cho Chun and La Eê are the two most remote communes of Nam Giang district with more than 90% of the population being ethnic minorities. Many areas do not have mobile phone signals, roads to commune centers and villages are very difficult. During the rainy season, you cannot travel by motorbike. Hunger and poverty still haunt the lives of our people here.",
+       introBlog2: "Each Tet gift includes 500,000 VND in cash and essential necessities such as warm clothes, rice, sugar, water. fish sauce, cooking oil, monosodium glutamate, milk, cakes, candy... not afraid of difficulties and obstacles, still overcoming mountains and forests to the poor people in the mountainous region of Nam Giang. Many people live on remote frontiers Unexpectedly receiving meaningful Tet gifts, I couldn't help but feel emotional.",
+       introBlog3: "On this occasion, the Volunteer Group visited, encouraged and gave Tet gifts to the Public Forces The official commune security is on duty to protect political security and ensure social order and safety in the country The 03 border communes mentioned above, each gift is worth 1 million VND.",
+       introBlog4: "Meaningful gifts before the New Year of the Tiger 2022 show the sincere feelings of the year Quang Nam police youth and sponsors share the hardships of people and contribute to bringing prosperity to a full, joyful and happy traditional Tet for the people in the border highlands."
+    },
+   jp: {
+       home: "ホーム",
+       activities: "アクティビティ",
+       achievement: "達成",
+       contributors: "貢献者",
+       QA: "QA & お問い合わせ", 
+       project_title: "ナムギャン県カジー郡パカン村の開発",
+       project_content:"パオン村のコツ族少数民族の持続可能な開発のための慈善事業は、コミュニティの生活の質を向上させるために、清潔な水、医療、教育、生計の機会を提供することを目的としています。 このプロジェクトは、持続可能な未来を創造し、恵まれない人々の生活に変化をもたらすことに焦点を当てています。 パオン村のコツ族少数民族は、清潔な水へのアクセスの欠如、不適切な医療施設、貧しい教育、他の基本的な生活必需品へのアクセスの制限など、さまざまな課題に直面しています。 慈善事業は、これらの課題を緩和し、これらの人々の生活の質を向上させることを目的としています。",
+       sponser_button: "貢献者またはスポンサーになる",
+       program_title:"村の子供たちに英語を教える",
+       program_content:"これは、地方の子供たちに英語教育を提供するための慈善事業の一環であるプログラムです。 このプログラムは、子供たちが英語で効果的にコミュニケーションを取るために必要なスキルを身につけることを目的としています。 経験豊富な教師チームが、子供たちに英語の基礎を教えます。",
+       achivement_slogan:"あなたが気にかけていることは何でも、それに取り組んでいる慈善団体があります。 慈善団体はさまざまな方法で助けてくれます",
+       achievement_sub:"慈善の実践とは、人道的な行為として、必要な人々に自発的に助けを与えることを意味します。 いくつかの哲学があります。",
+       achivement_title:"子供とドンバスからの難民のための資金調達",
+       achivement_button:"もっと見る",
+       blog_title:"私たちのブログ",
+       postBox_title:"ワームを持ってくる ナムザンの高地の人々に会いに行く",
+       postDecription: "これらはラオスのセコン県に隣接するコミューンで、チョーチュンとライーはナムザン地区で最も辺鄙なコミューンの2つで、人口の90％以上が少数民族で、その多くが少数民族です。 この地域には携帯電話の電波が届かず、コミューンセンターや村への道は非常に困難で、雨季にはバイクでの移動は不可能です。.",
+       introBlog:"2022年1月14～15日、ナムザン地区警察、省警察青少年組合の協力を得て、ホーチミン市人民の声、慈善団体リトルシスター・ボーとその仲間たちが協力し、2022年国境でボーダー・スプリング・プログラムを開催しました」 クアンナム省ナムザン地区のチョチュン、ラエ、ダクプリンの各自治体は、高地の人々に4億ドン以上相当の500個の贈り物を贈った",
+       introBlog1: "これらはラオスのセコン県に隣接するコミューンで、チョ・チュンとラ・イーは人口の90％以上が少数民族であるナムザン地区の最も辺鄙な2つのコミューンです。多くの地域では携帯電話の電波が届きません。 コミューンセンターや村への道は非常に困難です。雨季にはバイクで移動することはできません。飢餓と貧困が依然としてここの人々の生活を悩ませています。",
+       introBlog2: "各テトの贈り物には、現金 500,000 ドンと、暖かい衣類、米、砂糖、水などの必需品が含まれています。魚醤、食用油、グルタミン酸ナトリウム、牛乳、ケーキ、キャンディー...困難や障害を恐れることはありません。 ナムザン山岳地帯の貧しい人々に、山や森を乗り越えて届けました。多くの人が辺境の地に住んでいます。思いがけず意味のあるテトの贈り物を受け取り、感激せずにはいられませんでした。",
+       introBlog3: "この機会に、ボランティアグループは公的部隊を訪問し、奨励し、テトの贈り物を贈りました。コミューンの公式警備員は、政治的安全を守り、国内の社会秩序と安全を確保する義務があります。上記の3つの国境コミューン、それぞれの贈り物 100万VNDの価値があります。",
+       introBlog4: "2022年の寅年の新年を前に意味のある贈り物は、その年の誠実な気持ちを示しています。クアンナム警察の青年とスポンサーは人々の苦難を分かち合い、人々に充実した、楽しく幸せな伝統的なテットを繁栄させることに貢献します」 国境の高地。"
+    },
+   vn: {
+       home: "Trang chủ",
+       activities: "Hoạt động",
+       achievement: "Thành tựu",
+       contributors: "Nhà hảo tâm",
+       QA: "QA & Liên hệ",
+       project_title: "Phát triển làng Pa-Cang mới, xã Cà Dy, huyện Nam Giang",
+       project_content:"Dự án từ thiện vì sự phát triển bền vững của người dân tộc Cơ Tu tại làng Pà-Ong nhằm cung cấp nguồn nước sạch, chăm sóc sức khỏe, giáo dục và cơ hội sinh kế để cải thiện chất lượng cuộc sống cho cộng đồng. Dự án tập trung vào việc tạo ra một tương lai bền vững và tạo ra sự khác biệt trong cuộc sống của những người kém may mắn. Người dân tộc Cơ Tu tại làng Pà-Ong đối mặt với nhiều thách thức như thiếu nguồn nước sạch, cơ sở y tế không đầy đủ, giáo dục kém và hạn chế tiếp cận các tiện ích cơ bản khác. Dự án từ thiện nhằm giảm bớt những thách thức này và cải thiện chất lượng cuộc sống cho những người này.",
+       sponser_button: "Trở thành nhà hảo tâm hoặc nhà tài trợ",
+       program_title:"Dạy tiếng Anh cho trẻ em trong làng",
+       program_content:"Đây là một chương trình trong dự án từ thiện của chúng tôi nhằm cung cấp giáo dục tiếng Anh cho trẻ em ở vùng nông thôn. Chương trình được thiết kế để trang bị cho trẻ em những kỹ năng cần thiết để giao tiếp hiệu quả bằng tiếng Anh. Đội ngũ giáo viên giàu kinh nghiệm của chúng tôi sẽ dạy cho trẻ em những kiến thức cơ bản về tiếng Anh.",
+       achivement_slogan:"Bất kể điều gì bạn quan tâm, sẽ có một tổ chức từ thiện làm việc về nó. Các tổ chức từ thiện giúp đỡ theo nhiều cách khác nhau",
+       achievement_sub:"Thực hành từ thiện có nghĩa là việc tự nguyện cung cấp sự giúp đỡ cho những người có nhu cầu, như một hành động nhân đạo. Có một số triết lý.",
+       achivement_title:"Quyên góp cho trẻ em và người tị nạn từ Donbass",
+       achivement_button:"Xem thêm",
+       blog_title:"Tin tức",
+       postBox_title:"Mang Tết ấm áp đến người dân vùng cao Nam Giang",
+       postDecription: "Đây là những xã giáp tỉnh Se Kong, Lào, trong đó Chợ Chun và La Eê là hai xã vùng sâu vùng xa nhất của huyện Nam Giang với hơn 90% dân số là người dân tộc thiểu số, trong đó có nhiều người dân tộc thiểu số. Khu vực không có sóng điện thoại di động, đường đến trung tâm xã và các thôn rất khó khăn, vào mùa mưa không thể di chuyển bằng xe máy.",
+       introBlog:"Ngày 14-15/01/2022, được sự giúp đỡ của Công an huyện Nam Giang, Tỉnh Đoàn Thanh niên Công an tỉnh phối hợp với Đài Tiếng nói Nhân dân Thành phố Hồ Chí Minh, Hội Từ thiện Em Gái Bo và những người bạn tổ chức Chương trình Xuân Biên giới tại 03 biên giới các xã Chợ Chun, La Eê và Đăk Pring, huyện Nam Giang, tỉnh Quảng Nam trao tặng 500 phần quà trị giá hơn 400 triệu đồng cho bà con vùng cao.",
+       introBlog1: "Đây là các xã giáp tỉnh Se Kong, Lào, trong đó Chợ Chun và La Eê là 2 xã vùng sâu vùng xa nhất của huyện Nam Giang với hơn 90% dân số là người dân tộc thiểu số. Nhiều khu vực không có sóng điện thoại di động, Đường đến trung tâm xã, thôn rất khó khăn, vào mùa mưa xe máy không thể đi lại. Cái đói nghèo vẫn ám ảnh cuộc sống của người dân nơi đây.",
+       introBlog2: "Mỗi phần quà Tết bao gồm 500.000 đồng tiền mặt và các nhu yếu phẩm thiết yếu như quần áo ấm, gạo, đường, nước mắm, dầu ăn, bột ngọt, sữa, bánh, kẹo... không ngại khó khăn, trở ngại, vẫn vượt núi rừng đến với đồng bào nghèo miền núi Nam Giang. Nhiều người nơi biên giới xa xôi Bất ngờ nhận được những món quà Tết ý nghĩa, tôi không khỏi xúc động.",
+       introBlog3: "Nhân dịp này Đoàn tình nguyện đã đến thăm, động viên và tặng quà Tết cho lực lượng Công an xã biên giới làm nhiệm vụ bảo vệ an ninh chính trị, đảm bảo trật tự an toàn xã hội trên cả nước. 03 xã biên giới nêu trên, mỗi phần quà trị giá 1 triệu đồng.",
+       introBlog4: "Những món quà ý nghĩa trước Tết Nhâm Dần 2022 thể hiện tình cảm chân thành trong năm của thanh niên Công an Quảng Nam và các nhà tài trợ chia sẻ khó khăn của người dân, góp phần mang lại sự thịnh vượng cho một cái Tết cổ truyền trọn vẹn, vui tươi, hạnh phúc cho người dân trong nước cao nguyên biên giới."
+    }
+   };  
+
+   function changeLanguage(lang) {
+       var elements = document.querySelectorAll('[data-i18n]');
+       elements.forEach(function(element) {
+           var text = element.dataset.i18n;
+           element.innerHTML = translations[lang][text];
+       });
+      
+   }
